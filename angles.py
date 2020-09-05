6ff62f418b2a2ea5bd27b39f6c42dd4419960ae0
William Huang <whuang7337@gmail.com>
2020-09-05 03:36:36 -0700

    basic angle tool done

--- angles.py
+++ angles.py
@@ -48,6 +48,7 @@ class Application:
         self.canvas.unbind("<Motion>")
         self.canvas.bind("<Motion>", self.l_ghost_line)
         self.canvas.bind("<B1-Motion>", self.calc_angle)
+    
     def l_ghost_line(self, event):
         ghost_coords = (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
         
@@ -61,34 +62,7 @@ class Application:
                                 fill = "black", width = 5, tag = "first_line")
         
     def angle(self, cur_coords):
-        # slope of existing line
-        m = (self.mid_coords[1] - self.f_coords[1]) / (self.mid_coords[0] - self.f_coords[0])
-
-        # calculations done below are from the ax + by = c line form
-        # from the existing line
-        a = -m
-        b = 1
-        c = -self.mid_coords[1] + m * self.mid_coords[0]
-        
-        # coords of closest point on the line to cur coords
-        x = (b*(b*cur_coords[0] - a*cur_coords[1]) - a*c)/(a**2 + b**2)
-        y = (a*(-b*cur_coords[0] + a*cur_coords[1]) - b*c)/(a**2 + b**2)
-        
-        close_coords = (x,y)
-        
-        def dist(a, b):
-            dist = math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
-            return dist
-        
-        dist1 = dist(close_coords, cur_coords)
-        dist2 = dist(cur_coords, self.mid_coords)
-        
-        angle = math.degrees(math.acos(dist1/dist2))
-        return angle
-    
-    def calc_angle(self, event):
-        cur_coords = (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
-
+        left = True
         def atan_angle(a, x_sign, y_sign):
             degree = math.degrees(math.atan2(x_sign*(a[1] - self.mid_coords[1]), y_sign*(a[0] - self.mid_coords[0])))
             return degree
@@ -104,28 +78,44 @@ class Application:
         else:
             axis_angle = atan_angle(cur_coords, 1, -1)
             
+        if first_angle > 90:
+            first_angle = -(180 - first_angle)
+            left = True
+        elif first_angle < -90:
+            first_angle = -(-180 - first_angle)
+            left = True
+            
         if ((175 < self.prev_angle <= 180) & (-180 <= axis_angle < -175)) or ((175 < axis_angle <= 180) & (-180 <= self.prev_angle < -175)):
             if self.passed == False:
                 self.passed = True
             else:
                 self.passed = False
                 
-        if (self.passed == True) & (axis_angle <= 0):
+        if (self.passed == True) & (axis_angle - first_angle <= 0):
             a = 360
-        elif (self.passed == True) & (axis_angle > 0):
+        elif (self.passed == True) & (axis_angle - first_angle > 0):
             a = -360
         else: 
             a = 0 
-        
+            
         curr_angle = axis_angle + a - first_angle
         
-        if abs(curr_angle) >= 360:
+        if abs(curr_angle) >= 359.00:
             self.passed = False
             a = 0
-        
-        print(abs(curr_angle))
-        print(self.passed)
+            
         self.prev_angle = axis_angle
+        return abs(curr_angle)
+    
+    def calc_angle(self, event):
+        cur_coords = (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
+        curr_angle = self.angle(cur_coords)
+
+        self.canvas.delete("ghost")
+        self.canvas.create_line(cur_coords, self.mid_coords,
+                                fill = "gray", width = 5, tag = "ghost")
+        self.canvas.create_text(cur_coords[0] + 10, cur_coords[1] + 10, fill = "white", font = "Calibri 12",
+                                text = str(curr_angle), tag = "ghost", anchor = "nw")
 
 if __name__ == "__main__":
     root = tk.Tk()
