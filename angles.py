36d421bac73e760593f4fb3fd3d7258cfb712099
William Huang <whuang7337@gmail.com>
2020-09-05 03:35:48 -0700

    created angle algorithm and basic angle tool look

--- angles.py
+++ angles.py
@@ -21,6 +21,9 @@ class Application:
         # binds for initial line creation
         self.canvas.bind("<Button-1>", self.set_start)
         
+        self.prev_angle = 0
+        self.passed = False
+        
     def set_start(self, event):
         self.f_coords= (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
         
@@ -59,7 +62,6 @@ class Application:
         
     def angle(self, cur_coords):
         # slope of existing line
-        print(self.mid_coords, self.f_coords)
         m = (self.mid_coords[1] - self.f_coords[1]) / (self.mid_coords[0] - self.f_coords[0])
 
         # calculations done below are from the ax + by = c line form
@@ -68,22 +70,62 @@ class Application:
         b = 1
         c = -self.mid_coords[1] + m * self.mid_coords[0]
         
-        # distance of line from cur coords to existing line
-        distance1 = abs(a * cur_coords[0] + b * cur_coords[1] + c) / math.sqrt(a**2 + b**2)
+        # coords of closest point on the line to cur coords
+        x = (b*(b*cur_coords[0] - a*cur_coords[1]) - a*c)/(a**2 + b**2)
+        y = (a*(-b*cur_coords[0] + a*cur_coords[1]) - b*c)/(a**2 + b**2)
         
-        # distance from cur coords to mid point
-        x_diff = cur_coords[0] - self.mid_coords[0]
-        y_diff = cur_coords[1] - self.mid_coords[1]
-        distance2 = math.sqrt(x_diff**2 + y_diff**2)
-        angle = math.degrees(math.asin(distance1 / distance2))
+        close_coords = (x,y)
         
+        def dist(a, b):
+            dist = math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+            return dist
+        
+        dist1 = dist(close_coords, cur_coords)
+        dist2 = dist(cur_coords, self.mid_coords)
+        
+        angle = math.degrees(math.acos(dist1/dist2))
         return angle
     
     def calc_angle(self, event):
         cur_coords = (self.canvas.canvasx(event.x), self.canvas.canvasy(event.y))
+
+        def atan_angle(a, x_sign, y_sign):
+            degree = math.degrees(math.atan2(x_sign*(a[1] - self.mid_coords[1]), y_sign*(a[0] - self.mid_coords[0])))
+            return degree
         
-        angle = self.angle(cur_coords)
-        print(angle)
+        first_angle = atan_angle(self.f_coords, -1, 1)
+        
+        if (first_angle > 0) & (first_angle <= 90):
+            axis_angle = atan_angle(cur_coords, -1, 1)
+        elif (first_angle > 90) & (first_angle <= 180):
+            axis_angle = atan_angle(cur_coords, 1, -1)
+        elif (first_angle < 0) & (first_angle >= -90):
+            axis_angle = atan_angle(cur_coords, -1, 1)
+        else:
+            axis_angle = atan_angle(cur_coords, 1, -1)
+            
+        if ((175 < self.prev_angle <= 180) & (-180 <= axis_angle < -175)) or ((175 < axis_angle <= 180) & (-180 <= self.prev_angle < -175)):
+            if self.passed == False:
+                self.passed = True
+            else:
+                self.passed = False
+                
+        if (self.passed == True) & (axis_angle <= 0):
+            a = 360
+        elif (self.passed == True) & (axis_angle > 0):
+            a = -360
+        else: 
+            a = 0 
+        
+        curr_angle = axis_angle + a - first_angle
+        
+        if abs(curr_angle) >= 360:
+            self.passed = False
+            a = 0
+        
+        print(abs(curr_angle))
+        print(self.passed)
+        self.prev_angle = axis_angle
 
 if __name__ == "__main__":
     root = tk.Tk()
